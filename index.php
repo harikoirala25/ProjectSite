796925bc7f962f83f4ddced90e74bc2c62228951
webdevprogrammer <45622670+webdevprogrammer@users.noreply.github.com>
2020-04-30 00:18:52 -0600

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,189 @@
+<!-----------------------------------------------------------------------------------------
+                                    Home Page
+------------------------------------------------------------------------------------------->
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-	<title>Home</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>IT Support</title>
 </head>
 <body>
+	<div class="container-fluid">
+		<header>
+			<div class="header">
+				<h1>IT Support</h1>
+			</div>
+			<div class="nav">
+				<ul>
+					<li><a href="">Home</a></li>
+					<li><a href="">About Us</a></li>
+					<li><a href="">Our Services</a>
+						<ul>
+							<li><a href="">Networking</a></li>
+							<li><a href="">Web Development</a></li>
+							<li><a href="">Software Development</a></li>
+							<li><a href="">Hardware</a></li>
+							<li><a href="">Database Development</a></li>
+							<li><a href="">Android Development</a></li>
+						</ul>
+					</li>
+					<li><a href="">Contact Us</a></li>
+					<li><a href="">Feedback</a></li>
+				</ul>
+			</div>
+		</header>
 
+		<!-- Slider -->
+		<div class="slider">
+			Image slider goes here
+		</div>
+	</div>
+
+	<div class="container">
+
+		<!-- What we do -->
+		<h2>What we do...</h2>
+		<p>Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah 
+		Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah 
+		Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah </p>
+
+		<!-- Projects -->
+
+		<!-- Networking -->
+		<h3>Networking</h3>
+		<div class="card" style="width: 18rem;">
+		<img class="card-img-top" src="..." alt="Card image cap">
+		<div class="card-body">
+			<h5 class="card-title">Project Name</h5>
+			<p class="card-text">Project Description</p>
+			<a href="#" class="btn btn-primary">View Project</a>
+		</div>
+		</div>
+
+		<!-- Web Development -->
+		<h3>Web Development</h3>
+		<div class="card" style="width: 18rem;">
+		<img class="card-img-top" src="..." alt="Card image cap">
+		<div class="card-body">
+			<h5 class="card-title">Project Name</h5>
+			<p class="card-text">Project Description</p>
+			<a href="#" class="btn btn-primary">View Project</a>
+		</div>
+		</div>
+
+		<!-- Software Development -->
+		<h3>Software Development</h3>
+		<div class="card" style="width: 18rem;">
+		<img class="card-img-top" src="..." alt="Card image cap">
+		<div class="card-body">
+			<h5 class="card-title">Project Name</h5>
+			<p class="card-text">Project Description</p>
+			<a href="#" class="btn btn-primary">View Project</a>
+		</div>
+		</div>
+
+		<!-- Hardware -->
+		<h3>Hardware</h3>
+		<div class="card" style="width: 18rem;">
+		<img class="card-img-top" src="..." alt="Card image cap">
+		<div class="card-body">
+			<h5 class="card-title">Project Name</h5>
+			<p class="card-text">Project Description</p>
+			<a href="#" class="btn btn-primary">View Project</a>
+		</div>
+		</div>
+
+		<!-- Database Development -->
+		<h3>Database Development</h3>
+		<div class="card" style="width: 18rem;">
+		<img class="card-img-top" src="..." alt="Card image cap">
+		<div class="card-body">
+			<h5 class="card-title">Project Name</h5>
+			<p class="card-text">Project Description</p>
+			<a href="#" class="btn btn-primary">View Project</a>
+		</div>
+		</div>
+
+		<!-- Android App Development -->
+		<h3>Android App Development</h3>
+		<div class="card" style="width: 18rem;">
+		<img class="card-img-top" src="..." alt="Card image cap">
+		<div class="card-body">
+			<h5 class="card-title">Project Name</h5>
+			<p class="card-text">Project Description</p>
+			<a href="#" class="btn btn-primary">View Project</a>
+		</div>
+		</div>
+
+
+		<!-- Services -->
+
+		<div class="row">
+			<div class="col-4">
+				<!-- Networking -->
+				<h3>Networking</h3>
+				<img src="" alt="bla">
+				<p>blah blah blah <a href="#">View More</a></p>
+			</div>
+
+			<div class="col-4">
+				<!-- Web Development -->
+				<h3>Web Development</h3>
+				<img src="" alt="bla">
+				<p>blah blah blah <a href="#">View More</a></p>
+			</div>
+
+			<div class="col-4">
+				<!-- Software Development -->
+				<h3>Software Development</h3>
+				<img src="" alt="bla">
+				<p>blah blah blah <a href="#">View More</a></p>
+			</div>
+		</div>
+
+		<div class="row">
+			<div class="col-4">
+				<!-- Hardware -->
+				<h3>Hardware</h3>
+				<img src="" alt="bla">
+				<p>blah blah blah <a href="#">View More</a></p>
+			</div>
+
+			<div class="col-4">
+				<!-- Database Development -->
+				<h3>Database Development</h3>
+				<img src="" alt="bla">
+				<p>blah blah blah <a href="#">View More</a></p>
+			</div>
+
+			<div class="col-4">
+				<!-- Android Development -->
+				<h3>Android Development</h3>
+				<img src="" alt="bla">
+				<p>blah blah blah <a href="#">View More</a></p>
+			</div>
+		</div>
+	</div>
+
+	<!-- Footer Section -->
+    <footer>
+        <div class="row">
+            <div class="col-6">
+                <p><a href=""><img src="fblogo" alt="facebook logo"></a></p>
+                <p><a href=""><img src="fblogo" alt="twitter logo"></a></p>
+                <p><a href=""><img src="fblogo" alt="instagram logo"></a></p>
+                <p><a href=""><img src="fblogo" alt="linkend logo"></a></p>
+            </div>
+            <div class="col-6">
+                <p><a href="">Terms and Conditions</a></p>
+                <p><a href="">Privacy Policy</a></p>
+            </div>
+        </div>
+
+        <p>Copyright &amp; <a href="#">IT Support 2020</a></p>
+    </footer>
+	
 </body>
-</html> 
+</html>
+
