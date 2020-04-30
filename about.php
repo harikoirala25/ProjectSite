91b1075186058dc1d38f84fff726dc1b880486f8
webdevprogrammer <45622670+webdevprogrammer@users.noreply.github.com>
2020-04-30 00:20:56 -0600

    Update about.php

--- about.php
+++ about.php
@@ -1,9 +1,93 @@
+<!-----------------------------------------------------------------------------------------
+                                    About Page
+------------------------------------------------------------------------------------------->
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-	<title>About</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>IT Support</title>
 </head>
 <body>
-  <h1>About Us</h1>
+  <div class="container-fluid">
+
+  <!-- Header and Navigations -->
+    <header>
+      <div class="header">
+        <h1>IT Support</h1>
+      </div>
+      
+      <div class="nav">
+        <ul>
+          <li><a href="">Home</a></li>
+          <li><a href="">About Us</a></li>
+          <li><a href="">Our Services</a>
+            <ul>
+              <li><a href="">Networking</a></li>
+              <li><a href="">Web Development</a></li>
+              <li><a href="">Software Development</a></li>
+              <li><a href="">Hardware</a></li>
+              <li><a href="">Database Development</a></li>
+              <li><a href="">Android Development</a></li>
+            </ul>
+          </li>
+          <li><a href="">Contact Us</a></li>
+          <li><a href="">Feedback</a></li>
+        </ul>
+      </div>
+    </header>
+  </div>
+
+<!-- About Us -->
+  <div class="container">
+    <h2>About Us</h2>
+    <p>Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah 
+        Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah 
+        Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah 
+        Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah 
+    </p>
+
+    <p>Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah 
+        Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah 
+        Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah 
+        Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah 
+    </p>
+
+    <h2>Our Team</h2>
+    <div class="row">
+      <div class="col-6">
+        <h3>Ishor Bastola</h3>
+        <img src="" alt="Ishor">
+        <p>description about yourself</p>
+        <p><a href="">View More</a></p>
+      </div>
+
+      <div class="col-6">
+        <h3>Hari Koirala</h3>
+        <img src="" alt="Hari">
+        <p>description about yourself</p>
+        <p><a href="">View More</a></p>
+      </div>
+    </div>
+
+  </div>
+
+	<!-- Footer Section -->
+  <footer>
+    <div class="row">
+      <div class="col-6">
+        <p><a href=""><img src="fblogo" alt="facebook logo"></a></p>
+        <p><a href=""><img src="fblogo" alt="twitter logo"></a></p>
+        <p><a href=""><img src="fblogo" alt="instagram logo"></a></p>
+        <p><a href=""><img src="fblogo" alt="linkend logo"></a></p>
+      </div>
+      <div class="col-6">
+        <p><a href="">Terms and Conditions</a></p>
+        <p><a href="">Privacy Policy</a></p>
+      </div>
+    </div>
+    <p>Copyright &amp; <a href="#">IT Support 2020</a></p>
+  </footer>
 </body>
 </html>
+
