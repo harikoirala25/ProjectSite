d0d1e29d09b04d09943fd69825581535b86e245a
webdevprogrammer <45622670+webdevprogrammer@users.noreply.github.com>
2020-05-04 23:35:23 -0600

    Update contact.php

--- Documents/contact.php
+++ Documents/contact.php
@@ -1,42 +1,74 @@
+
 <!-----------------------------------------------------------------------------------------
                                     Contact Page
 ------------------------------------------------------------------------------------------->
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-	<link rel="stylesheet" type="text/css" href="Style.css">
+	    <script
+      src="https://code.jquery.com/jquery-3.4.1.slim.min.js"
+      integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n"
+      crossorigin="anonymous"
+    ></script>
+    <script
+      src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
+      integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo"
+      crossorigin="anonymous"
+    ></script>
+    <script
+      src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"
+      integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6"
+      crossorigin="anonymous"
+    ></script>
+    <link
+      rel="stylesheet"
+      href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+      integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh"
+      crossorigin="anonymous"
+    />
+	<link rel="stylesheet" type="text/css" href="../style.css" />
 	<title>IT Support</title>
 </head>
 <body>
-	<div class="container-fluid">
-		<header>
-			<div class="header">
-				<h1>IT Support</h1>
-			</div>
-			<div class="nav">
-				<ul>
-					<li><a href="">Home</a></li>
-					<li><a href="">About Us</a></li>
-					<li><a href="">Our Services</a>
-						<ul>
-							<li><a href="">Networking</a></li>
-							<li><a href="">Web Development</a></li>
-							<li><a href="">Software Development</a></li>
-							<li><a href="">Hardware</a></li>
-							<li><a href="">Database Development</a></li>
-							<li><a href="">Android Development</a></li>
-						</ul>
-					</li>
-					<li><a href="">Contact Us</a></li>
-					<li><a href="">Feedback</a></li>
-				</ul>
-			</div>
-		</header>
-    </div>
+		    <nav class="navbar navbar-expand-sm nav-size navbar-design">
+      <!-- Logo -->
+      <a class="navbar-brand" href="#">IT Support</a>
+      <!-- Navigation -->
+      <ul class="navbar-nav ml-auto">
+        <li class="nav-item active">
+          <a class="nav-link" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">About Us</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a
+            class="nav-link dropdown-toggle"
+            href="#"
+            id="navbardrop"
+            data-toggle="dropdown"
+          >
+            Our Services
+          </a>
+          <div class="dropdown-menu">
+            <a class="dropdown-item" href="#">Networking</a>
+            <a class="dropdown-item" href="#">Web Development</a>
+            <a class="dropdown-item" href="#">Software Development</a>
+            <a class="dropdown-item" href="#">Hardware</a>
+            <a class="dropdown-item" href="#">Database Development</a>
+            <a class="dropdown-item" href="#">Android Development</a>
+          </div>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Contact Us</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Feedback</a>
+        </li>
+      </ul>
+    </nav>
 
 
 
