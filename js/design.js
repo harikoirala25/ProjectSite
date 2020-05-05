f2aa93031a1cf9e888cf566b876338ecf646b917
webdevprogrammer <45622670+webdevprogrammer@users.noreply.github.com>
2020-05-04 23:23:11 -0600

    Update design.js

--- js/design.js
+++ js/design.js
@@ -1,1 +1,8 @@
-
+$(document).ready(function() {
+  $(".nav-link").click(function() {
+    $(".nav-link").css("background-color", "");
+    $(this).css("background-color", "#4B7FBB");
+    $(this).css("border-radius", "4px");
+    $(this).css("transition", "all 2s");
+  });
+});
