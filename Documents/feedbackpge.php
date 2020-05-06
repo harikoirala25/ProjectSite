4c23b89eb09bd1c23d38e0e6bba0b13ac680be0e
Hari <57735337+harikoirala25@users.noreply.github.com>
2020-05-05 21:57:21 -0600

    Update feedbackpge.php

--- Documents/feedbackpge.php
+++ Documents/feedbackpge.php
@@ -23,16 +23,19 @@ if(isset($_POST['submit'])){
 	$money = $_POST['money'];
 	$textarea = $_POST['textarea'];
 
+	//inserting data in to the databse 
 	if (isset($services) ||isset($site) ||isset($overall) ||isset($expectation) ||isset($easy) ||isset($needs) ||isset($describe) ||isset($money) ||isset($textarea))
 	{
 		$sql = "INSERT INTO feedback (satisfied,sitenav,overall,expectations,information,needs,description,moneyvalue,comments)
 		VALUES ('$services', '$site', '$overall', '$expectation', '$easy', '$needs', '$describe', '$money', '$textarea')";
 	}
-
+     
+     //if connection is success redirect to thankyou pge
 	if ($conn->query($sql) === TRUE)
 	{
          header("Location:thankyou.php");
 	}
+	//if there is a problem redirect them to the index pge 
 	else
 	{
 		header("Location:index.php?ConnectionError");
